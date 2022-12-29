f7625a12f9b878a9668ab337bc197df61317926b
Denes-cilwal <dinesh.silwal@wesionary.team>
2022-12-29 20:04:52 +0545

    problems pairs in array solved

--- lectures/07-Array/PairsInArray.java
+++ lectures/07-Array/PairsInArray.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class PairsInArray {
+public class PairsInArray {
+
+    public static void main(String[] args) {
+        /*
+        (2,4)(2,6)(2,8)(2,10)
+        (4,6)(4,8)(4,10)
+        (6,8)(6,10)
+        (8,10)
+         */
+        int[] arrNum = {2,4,6,8,10};
+        int result = printParis(arrNum);
+        System.out.println(result);
+
+    }
+    static  int printParis(int[] num){
+        for(int i = 0; i < num.length; i++){
+            int curr =  num[i];
+                for (int j = i+1 ; j<num.length; j ++){
+                    System.out.print("(" + curr + "," + num[j] + ")");
+                }
+            System.out.println();
+        }
+        return 0;
+    }
+
 }
