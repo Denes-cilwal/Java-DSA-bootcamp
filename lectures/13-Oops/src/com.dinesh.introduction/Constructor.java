e970704d2fdd4c361584aae1429e6465b3864955
Denes-cilwal <dinesh.silwal@wesionary.team>
2023-06-03 20:36:08 +0545

    oops concepts - class, object , constructors, WrapperClass

---
+++ lectures/13-Oops/src/com.dinesh.introduction/Constructor.java
@@ -0,0 +1,114 @@
+public class Constructor {
+    public static void  main(String[] args){
+    Employee1 emp1  = new Employee1();
+    // Employee1() is a special function inside the class
+    // But we did not create any function inside the class
+        // but where is this Employee1()  being called - this is called default
+        // Constructor is a special function that runs when you create object, and it allocates
+        // some varibales
+
+        // why we want to make our own constructors and bind the args to objects
+
+        System.out.println(emp1.name);
+        System.out.println(emp1.roomNo);
+        System.out.println(emp1.salary);
+        emp1.changeName("TechTa");
+        emp1.greeting();
+//        emp1.changeName("TechTa");
+
+
+      Employee1 emp2 = new Employee1(13,"david" , 234567f);
+        System.out.println(emp2.name);
+        System.out.println(emp2.roomNo);
+        System.out.println(emp2.salary);
+
+
+     Employee1 random  = new Employee1(emp2);
+        System.out.println(random.name);
+        System.out.println(random.roomNo);
+        System.out.println(random.salary);
+    }
+
+    Employee1 random2  = new Employee1();
+}
+
+class Employee1 {
+    // instance variables - inside class outside method
+    int roomNo;
+    String name;
+    float salary ;
+
+    // we need a way to add the values of the above properties obj by obj
+    // we need one word to access every object.
+    /*
+    Employee1(){
+        // this is constructor
+        // this run when you create new obj
+
+        this.roomNo means internally    => emp.roomNo means internally taking place
+           emp1.name = "";
+        emp1.salary = 23445667.89;
+
+    }
+     */
+
+   void greeting(){
+       // this is doing emp1.name == this.name
+       // self keyword in python == this
+        System.out.println("Hello! My Name is " + this.name);
+
+
+    }
+
+    // method
+
+    void changeName (String newName){
+        this.name =  newName;
+    }
+    // constructor call - default
+/*
+    Employee1(){
+
+        this.roomNo = 13;
+        this.name = "des";
+        this.salary = 23445667.89f;
+
+    }
+*/
+    // parameterized
+    Employee1(int newNo, String newName, float newSalary){
+        this.roomNo = newNo;
+        this.name = newName;
+        this.salary = newSalary;
+    }
+
+    // same method name but functionality method overloading
+    // constructor overloading
+    // polymorphism
+
+    Employee1(Employee1 emp3){
+       this.name =  emp3.name;
+       this.roomNo = emp3.roomNo;
+       this.salary = emp3.salary;
+    }
+
+
+
+    // calling constructor from another constructor
+    Employee1(){
+        this(10, "ghu",100.0f );
+        // equivalent to Student(10, "ghu",100.0f ) =
+        // internally => new Employee1(13,"david" , 234567f);
+    }
+
+}
+// this refers to what object you are referring to
+// primitives are not objects, so they are not stored in heap they are stored in stack
+// that's why we do not use new keyword for primitives and stack memory allocation is fast
+// that's why java is slow heap memory objects
+// python is also slow
+
+
+//    Employee1 emp1  = new Employee1();
+//emp2  = emp1
+// change by emp1 also changes emp2 as pointing to same object in heap
---
+++ lectures/13-Oops/src/com.dinesh.introduction/Main.java
@@ -0,0 +1,88 @@
+
+public class Main {
+    public static  void main(String[] args) {
+        // case : I - create a data type that stores 5 roll no's
+
+        // to store 5 roll no you can create array data structure
+
+        int[] numbers = new int[5];
+
+        // store 5 name
+        String[] names =  new String[5];
+
+        // create a data type to store data of 5 student , now the complexity arises here
+        // what are the data types included for student
+
+        // data of 5 students : {roll no , name , marks}
+        int[] rno = new int[5];
+        String[] name = new  String[5];
+        float[] marks =   new float[5];
+        // data includes all three fields roll no name and marks but all are different
+        // I want to create data structure that store all three students that where classes and object concept comes in
+
+        // ---------------------------------********************--------------------------------------------------------
+
+        // A class is named group of function and properties
+        // By convention class starts with Cap letter
+
+//        Student[] students = new Student[5];
+    
+        // just declaring
+//        Student students;
+//        students = new Student();
+        // System.out.println(students); // this is just null
+
+
+     }
+
+     // create a class , class is like logical construct a template
+        // a real life example of Class is Car, using this Car they are creating audi, bmw, swift
+        // A class properties like petrol, engine, price are same, but they all differ in value.
+        // when you declare object of class you are creating instance(physical) of class.
+
+
+    // class is logical construct
+    // object is physical reality // that is something occupying space in memory (following template and creating physical instances of it).
+
+
+    class  Student {
+         int rno ;
+         String name ;
+         float marks ;
+     }
+
+
+     // object are stored in heap memory and reference variables in stack memory
+     // suppose we have class and three properties, and every object of class will have those properties
+     // But how to access students properties ? ? ? ?
+     //   -  The ans is using dot operator
+
+
+    // instance variables -  name of the instance
+    // dot operator - linking reference variable with instance variable(outside the method but inside the class).
+
+
+    // new keywords
+    // -  Student student1 ;  // declare reference variable it is not creating object, just reference to object of type Class - just in stack memory
+    // new - dynamically allocates memory at run time and returns a reference to it .
+
+//    student student1;
+//    student1   = new Student(); // student1 will have reference on it.
+
+    
+        // student student1 =  new student();
+//         compile time           run time
+
+
+    // compile time is program is checking where code has error, byte code  to machine code conversion
+    // when all this happens, it is run time allocating memory - this is dynamic memory allocation
+
+
+    // static type language - suppose you have only three properties that is roll no , name , marks
+    // now you create object and that object trying to access that properties of class
+    // but if you try to access properties that is not present in the class just like kunal.salary
+    // then it cant happens, this is why java is called static type language
+    // you cant change properties in run time - dynamic type.
+    // but dynamic can be done in python language
+
+}
