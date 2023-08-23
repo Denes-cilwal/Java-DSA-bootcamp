cedf4e5ae5ea296df948b68b482c350c1f1c30e8
Denes-cilwal <dinesh.silwal@wesionary.team>
2023-08-23 21:35:19 +0545

    java code strgs cli concepts

--- lectures/02-First-java-program/JavaBasics.java
+++ lectures/02-First-java-program/JavaBasics.java
@@ -2,6 +2,9 @@
 public class JavaBasics {
     public static void main(String[] args) {
 //        System.out.print("Hello World\nHello");
+        // whatever the data you give in terminal it will appear in the console
+        //  java JavaBasics 20 "dnes"
+        // 20 is in 0 index, "dnes is in 1 index"
         System.out.println(args[1]);
     }
 }
