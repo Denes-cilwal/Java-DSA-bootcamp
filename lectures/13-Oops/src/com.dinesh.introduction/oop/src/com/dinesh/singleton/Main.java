ba09143bbc9b38a8b7c6e3ed3e3cd67fcdc26586
Denes-cilwal <dinesh.silwal@wesionary.team>
2023-09-05 08:29:59 +0545

    singleton design pattern

--- lectures/13-Oops/src/com.dinesh.introduction/oop/src/com/dinesh/singleton/Main.java
+++ lectures/13-Oops/src/com.dinesh.introduction/oop/src/com/dinesh/singleton/Main.java
@@ -4,7 +4,12 @@ import oop.src.com.dinesh.accessControl.A;
 
 public class Main {
     public static void main(String[] args) {
-       //  Singleton obj = new Singleton(); // is not allowed since it is private
+        // Singleton obj = new Singleton(); // is not allowed since it is private
+
+        /*
+        When a constructor (or any method or variable) is declared as private in a class, it means that it cannot be directly accessed from outside that class, including from the main method or any other class,
+         even if that main method resides within the same class.
+         */
         Singleton obj =  Singleton.getInstance();
         Singleton obj1 =  Singleton.getInstance();
         Singleton obj2 =  Singleton.getInstance();
--- lectures/13-Oops/src/com.dinesh.introduction/oop/src/com/dinesh/singleton/Singleton.java
+++ lectures/13-Oops/src/com.dinesh.introduction/oop/src/com/dinesh/singleton/Singleton.java
@@ -4,14 +4,23 @@ public class Singleton {
     // it just classes where only one object is allowed to create
     // when we create only 1 obj, you should not allow anyone  call construct
     // everytime constructor is called new object is created
+    // you task is to avoid calling constructor
 
-    private int num = 0;
-    // private means it will be used in this class only
-    private Singleton(){}
+//    private int num = 0;
+    // private means it will be used in this class only cannot be accessed from main method as well
+    private Singleton(){} //  Private constructor ensures no one else can create a new object of Singleton
 
+
+    // returning only single instance
+    // after first time object is created object is created here which is pointing to obj in main class
     private static Singleton instance;
+
+    // create a function that gives only single instance
     public static Singleton getInstance() {
+        // why static here, because class is accessing ti
         // check whether 1 obj is created or not
+        // you're accessing the getInstance() method directly on the class, not on an instance.
+        // This is possible because the method is static.
         if (instance == null){
             instance =  new Singleton();
         }
